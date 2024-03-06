a899eae996a3709b3b826f7bf650c70cf473978a
Tez Wright <tez.corp67@gmail.com>
2024-03-06 15:05:06 -0600

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Cart from "./Cart"
 import Nav from "./Nav"
+import Footer from "./Footer"
 
 const App = () => {
   return (
     <div>
       <Nav/>
+      <Cart/>
+      <Footer/>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
